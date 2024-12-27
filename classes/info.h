2b1272bdfe29633b5475d2da4f75f84e38930959
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2024-12-27 15:35:26 +0100

    Added different screen sizes compatibility for the regular m5stickc

--- classes/info.h
+++ classes/info.h
@@ -60,16 +60,28 @@ public:
 	void renderRepository() {
 		SCREEN.setTextColor(WHITE, BLACK);
 
-		SCREEN.setTextSize(2);
-		SCREEN.setCursor(10, 40);
-		SCREEN.println("Repository:");
-		SCREEN.setTextSize(1);
-		SCREEN.setCursor(10, 80);
-		SCREEN.println("https://github.com");
-		SCREEN.setCursor(10, 90);
-		SCREEN.println("/IMXNOOBX/fstick");
+		#if !defined(PLUS) && !defined(PLUS2)
+			SCREEN.setTextSize(1);
+			SCREEN.setCursor(5, 20);
+			SCREEN.println("Repository:");
+			SCREEN.setCursor(5, 35);
+			SCREEN.println("github.com/");
+			SCREEN.setCursor(5, 45);
+			SCREEN.println("IMXNOOBX/fstick");
 
-		SCREEN.qrcode("https://github.com/IMXNOOBX/fstick", SCREEN_WIDTH - 85, SCREEN_HEIGHT - 85, 80, 5);
+			SCREEN.qrcode("https://github.com/IMXNOOBX/fstick", 5, SCREEN_HEIGHT - 65, 60, 4);
+		#else
+			SCREEN.setTextSize(2);
+			SCREEN.setCursor(10, 40);
+			SCREEN.println("Repository:");
+			SCREEN.setTextSize(1);
+			SCREEN.setCursor(10, 80);
+			SCREEN.println("https://github.com");
+			SCREEN.setCursor(10, 90);
+			SCREEN.println("/IMXNOOBX/fstick");
+
+			SCREEN.qrcode("https://github.com/IMXNOOBX/fstick", SCREEN_WIDTH - 85, SCREEN_HEIGHT - 85, 80, 5);
+		#endif
 
 		SCREEN.setCursor(10, SCREEN_HEIGHT - 20);
 		SCREEN.println("Click to return");
@@ -78,11 +90,21 @@ public:
 	void renderDonate() {
 		SCREEN.setTextColor(WHITE, BLACK);
 
-		SCREEN.setTextSize(1);
-		SCREEN.setCursor(5, 20);
-		SCREEN.println("Please consider donating so i can keep improving this project and get a flipper zero to work on more projects :D");
+		#if !defined(PLUS) && !defined(PLUS2)
+			SCREEN.setTextSize(1);
+			SCREEN.setCursor(5, 20);
+			SCREEN.println("Please donate to help");
+			SCREEN.setCursor(5, 30);
+			SCREEN.println("improve this project!");
 
-		SCREEN.qrcode("https://www.paypal.com/donate/?business=imxnoobx@gmail.com&cmd=_donations", SCREEN_WIDTH - 85, SCREEN_HEIGHT - 85, 80, 5);
+			SCREEN.qrcode("https://www.paypal.com/donate/?business=imxnoobx@gmail.com&cmd=_donations", 5, SCREEN_HEIGHT - 65, 60, 4);
+		#else
+			SCREEN.setTextSize(1);
+			SCREEN.setCursor(5, 20);
+			SCREEN.println("Please consider donating so i can keep improving this project and get a flipper zero to work on more projects :D");
+
+			SCREEN.qrcode("https://www.paypal.com/donate/?business=imxnoobx@gmail.com&cmd=_donations", SCREEN_WIDTH - 85, SCREEN_HEIGHT - 85, 80, 5);
+		#endif
 
 		SCREEN.setTextSize(1);
 		SCREEN.setCursor(10, SCREEN_HEIGHT - 20);
